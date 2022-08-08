4afedcf6ac19620aa88d22b60c6fa3ecafd9058d
Štěpán Šimek <simekst2@fit.cvut.cz>
2022-08-08 09:11:55 +0200

    RTP_EXPORTER: header file update (dependencies)

--- process/rtp.hpp
+++ process/rtp.hpp
@@ -57,8 +57,6 @@
 # include <ipfixprobe/packet.hpp>
 # include <ipfixprobe/ipfix-elements.hpp>
 
-# include <devel/endian_mac.h>
-
 namespace ipxp {
 # define RTP_HEADER_MINIMUM_SIZE            12
 
