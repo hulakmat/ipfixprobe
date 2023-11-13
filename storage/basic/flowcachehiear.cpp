2aa92cdf516907025f0d8ab2ac6eed6960dedd1b
Tomas Benes <benesto3@fit.cvut.cz>
2023-11-13 06:26:00 +0100

    feat: Stats wrapper to better name exported statistics

--- storage/basic/flowcachehiear.cpp
+++ storage/basic/flowcachehiear.cpp
@@ -5,12 +5,39 @@
 #include "hiearchyflowstore.hpp"
 #include "cachedflowstore.hpp"
 #include "flowstorestatswriter.hpp"
+#include "flowstorepacketindexer.hpp"
+
+#include <boost/core/demangle.hpp>
+
+
+extern "C" {
+struct FilteredStore {};
+struct BaseStore {};
+}
 
 namespace ipxp {
+
+template <typename F, typename Label>
+class FlowStoreStatsWrapper : public FlowStoreProxySimple<F>
+{
+public:
+    typedef typename F::packet_info PacketInfo;
+    typedef typename F::accessor Access;
+    typedef typename F::iterator Iter;
+    typedef typename F::parser Parser;
+    
+    FlowStoreStat::Ptr stats_export() {
+        auto ptr = this->m_flowstore.stats_export();
+        std::string label = boost::core::demangle(typeid(Label).name());
+        ptr->setName(label);
+        return ptr;
+    };
+};
 
 Plugin *cons_cached_storage_func()
 {
     return new ipxp::FlowCache<
+            FlowStorePacketIndexer<
             FlowStoreStatsWriter<
                 FlowStoreMonitor<
                     FlowStoreCached<
@@ -24,6 +51,7 @@ Plugin *cons_cached_storage_func()
                         >
                     >
                 >
+            >
             >
             >("cachedStorage");
 };
